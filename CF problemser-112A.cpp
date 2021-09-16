3ef3ee47c988b6660ca8dd7543dff0eccfe1f1e6
BHARGAVI.V <74961558+Bhar-02@users.noreply.github.com>
2021-09-16 15:55:33 +0530

    CF problemset-112A

---
+++ CF problemser-112A.cpp
@@ -0,0 +1,23 @@
+//Codeforces Problemset-112A
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+	string s1,s2;
+	cin>>s1;
+	cin>>s2;
+	int len=s1.length();
+	for (int i=0;i<len;i++){
+		if (isupper(s1[i])){
+			s1[i]=tolower(s1[i]);
+		}
+	}
+	for (int i=0;i<len;i++){
+		if (isupper(s2[i])){
+			s2[i]=tolower(s2[i]);
+		}
+	}
+	if (s1==s2) cout<<0<<endl;
+	else if (s2<s1) cout<<1<<endl;
+	else if (s1<s2) cout<<-1<<endl;
+}

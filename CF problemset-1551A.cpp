8cc358b27b2c17b20276b75104c90c0821e91269
BHARGAVI.V <74961558+Bhar-02@users.noreply.github.com>
2021-09-16 16:01:28 +0530

    CF problemset-1551A.cpp

---
+++ CF problemset-1551A.cpp
@@ -0,0 +1,23 @@
+//Codeforces Problemset-1551A
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while (t--){
+		int n;
+		cin>>n;
+		int a=n/3;
+		if (n%3==0){
+			cout<<a<<" "<<a<<endl;
+		}
+		if (n%3==1){
+			cout<<a+1<<" "<<a<<endl;
+		}
+		if (n%3==2){
+			cout<<a<<" "<<a+1<<endl;
+		}
+	}
+}
